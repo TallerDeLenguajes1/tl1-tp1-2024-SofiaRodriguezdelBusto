60c55655cfa5548ee27da406393ea8e4dca2c054
Sofia Rodriguez del Busto <rdbsofi@gmail.com>
2024-03-29 16:40:41 -0300

    Ejercicio 2.3.e introducción a punteros

--- tp1__1.cpp
+++ tp1__1.cpp
@@ -3,5 +3,12 @@
 int main()
 {
     printf("Hola Mundo \n");
+    int num = 5, *punt;
+    punt = &num;
+    printf("\nContenido del puntero: %d", *punt);
+    printf("\nDirección de memoria almacenada por el puntero: %p", punt);
+    printf("\nDirección de memoria de la variable: %p", &num);
+    printf("\nDirección de memoria del puntero: %p", &punt);
+    printf("\nEspacio de memoria utilizado por la variable: %zu bytes", sizeof(num));  
     return 0;
 }
